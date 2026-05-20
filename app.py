e6b3b702319b3b39d47a49a40d80ec307e51a2e5
ggggmin <smjsky0511@gmail.com>
2026-05-20 16:28:22 +0900

    Implement a to-do list feature in Streamlit app

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
 import streamlit as st
-st.title('천안오성고 1학년 3반 화이팅')
-st.write('기술가정수행 잘하자!')
+
+# 제목
+st.title("📝 투두리스트")
+
+# 할 일 저장 공간 만들기
+if "todos" not in st.session_state:
+    st.session_state.todos = []
+
+# 입력창
+todo = st.text_input("할 일을 입력하세요")
+
+# 추가 버튼
+if st.button("추가"):
+    if todo != "":
+        st.session_state.todos.append(todo)
+
+# 목록 출력
+st.subheader("📌 할 일 목록")
+
+for i, item in enumerate(st.session_state.todos):
+    col1, col2 = st.columns([5, 1])
+
+    with col1:
+        st.write(f"{i + 1}. {item}")
+
+    with col2:
+        if st.button("삭제", key=i):
+            st.session_state.todos.pop(i)
+            st.rerun()
